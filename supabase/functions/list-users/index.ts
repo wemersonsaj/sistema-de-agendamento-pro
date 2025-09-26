90841f18f6481aee6283794fe7048a5bdb0f2875
[dyad] <agapiceoficial@gmail.com>
2025-09-26 16:49:48 -0300

    [dyad] Corrigindo erros de tipo do Deno - wrote 2 file(s)

--- supabase/functions/list-users/index.ts
+++ supabase/functions/list-users/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="https://esm.sh/@types/deno" />
+
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts"
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0'
 
