febb29ced2e1ef37b3bd595f00993f3b6d3d9f1b
[dyad] <agapiceoficial@gmail.com>
2025-09-26 16:58:18 -0300

    [dyad] Corrigindo erros de tipo do TypeScript nas Edge Functions - wrote 2 file(s)

--- supabase/functions/list-users/index.ts
+++ supabase/functions/list-users/index.ts
@@ -1,7 +1,9 @@
-/// <reference types="https://esm.sh/@types/deno" />
-
+// @ts-ignore
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts"
+// @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0'
+
+declare const Deno: any;
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
@@ -27,7 +29,7 @@ async function verifyUser(req: Request) {
     return user;
 }
 
-serve(async (req) => {
+serve(async (req: Request) => {
   if (req.method === 'OPTIONS') {
     return new Response(null, { headers: corsHeaders })
   }
